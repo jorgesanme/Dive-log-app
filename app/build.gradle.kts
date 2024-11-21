8f4bf725baf90b9f5a3c4f53996302e318394c5b
jorgesname <jorgesanme@gmail.com>
2024-11-21 10:26:53 +0000

    fix compileSdk

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -5,7 +5,7 @@ plugins {
 
 android {
     namespace = "com.smedina.dive_log_app"
-    compileSdk = 34
+    compileSdk = 35
 
     defaultConfig {
         applicationId = "com.smedina.dive_log_app"
